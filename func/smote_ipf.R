7badbd23eab586195c7e2eee7ae5ab2b0ca15505
ldanov <ldanov@users.noreply.github.com>
2019-01-13 21:21:55 +0100

    changed arg names to fit other functions

--- func/smote_ipf.R
+++ func/smote_ipf.R
@@ -1,10 +1,10 @@
 #################################
 ####### Safelevel - SMOTE #######
-## data_origin - training dataset (with SMOTE already applied)
-## target - target variable
+## data - training dataset (with SMOTE already applied)
+## colname_target - target variable
 ## n - splits in each iteration
 ## k - stopping_rounds where rejected obs<=p
-## p - percentage from data_origin to use as stopping_metric
+## p - percentage from data to use as stopping_metric
 ## Defaults for n, k, p taken from original algorithm paper
 ## For details see Sáez, J. A., Luengo, J., Stefanowski, J., & Herrera, F. (2015). 
 ## SMOTE-IPF: Addressing the noisy and borderline examples problem in imbalanced 
@@ -13,7 +13,7 @@
 ## Lyubomir Danov, 2018
 #################################
 
-smote_ipf <- function(data_origin, target, n=9, k=3, p=0.01, silent=TRUE) {
+smote_ipf <- function(data, colname_target, n=9, k=3, p=0.01, silent=TRUE) {
   try_rweka <- require(RWeka)
   try_dplyr <- require(dplyr)
   missing_lib <- toString(deparse(substitute(try_rweka)), deparse(substitute(try_dplyr)))
@@ -23,53 +23,52 @@ smote_ipf <- function(data_origin, target, n=9, k=3, p=0.01, silent=TRUE) {
   }
   
   # Currently does not handle any NAs
-  if(anyNA(data_origin)) {
-    stop("data_origin contains NA values")
+  if(anyNA(data)) {
+    stop("data contains NA values")
   }
   
-  if(missing(data_origin) | missing(target)) {
-    stop("missing data_origin or target")
+  if(missing(data) | missing(colname_target)) {
+    stop("missing data or colname_target")
   }
   
-  colclasses <- sapply(data_origin, class)
+  colclasses <- sapply(data, class)
   categorical <- names(colclasses)[colclasses %in% c("character", "factor")]
   char2fac <- names(colclasses)[colclasses %in% c("character")]
   for (conv in char2fac) {
-    if (class(data_origin[,conv])=="character") data_origin[,conv] <- as.factor(data_origin[,conv])
+    if (class(data[,conv])=="character") data[,conv] <- as.factor(data[,conv])
   }
   
   # initialise
-  E <- data_origin
-  E$row_id <- 1:nrow(E)
-  colnames(E)[colnames(E)==target] <- "smote_ipf_target"
+  data$row_id <- 1:nrow(data)
+  colnames(data)[colnames(data)==colname_target] <- "smote_ipf_target"
   k_i <- 1
   eval_k_i <- TRUE
   
   # create model formula
-  features <- colnames(data_origin)[colnames(data_origin)!=target]
+  features <- colnames(data)[colnames(data)!=colname_target]
   full_formula <- as.formula(paste0("smote_ipf_target", " ~ ", paste(features, collapse = " + ")))
   
   while(eval_k_i) {
-    # first rearrange data_origin via sample
+    # first rearrange data via sample
     # then rep_len(c(1:n), length.out = n())
     set.seed(k_i)
-    E <- E[sample(x = 1:nrow(E), replace = FALSE), ]
-    E$split_k_i <- rep_len(c(1:n), length.out = nrow(E))
+    data <- data[sample(x = 1:nrow(data), replace = FALSE), ]
+    data$split_k_i <- rep_len(c(1:n), length.out = nrow(data))
     
     for (n_i in 1:n) {
-      E_n_i <- E[E$split_k_i==n_i,]
+      E_n_i <- data[data$split_k_i==n_i,]
       c45_n_i <- J48(formula=full_formula, data=E_n_i, 
                      control = Weka_control())
       
-      predict_n_i <- E %>%
+      predict_n_i <- data %>%
         select(one_of("smote_ipf_target", "row_id")) %>% 
         mutate(model_n_i=n_i) %>%
-        mutate(label=predict(c45_n_i, newdata=E, type="class")) 
+        mutate(label=predict(c45_n_i, newdata=data, type="class")) 
       
       if(n_i==1) {
         predict_k_i <- predict_n_i
       } else {
-        predict_k_i <- predict_k_i %>% bind_rows(predict_n_i)
+        predict_k_i <- bind_rows(predict_k_i, predict_n_i)
       }
       
       rm(E_n_i, c45_n_i, predict_n_i)
@@ -81,8 +80,8 @@ smote_ipf <- function(data_origin, target, n=9, k=3, p=0.01, silent=TRUE) {
       summarise(votes=sum(eval_predict)) %>%
       filter(votes<(n/2))
     
-    p_k_i <- nrow(cast_vote_k_i)/nrow(data_origin)
-    E <- E %>% 
+    p_k_i <- nrow(cast_vote_k_i)/nrow(data)
+    data <- data %>% 
       anti_join(cast_vote_k_i, by="row_id")
     
     if(k_i==1) {
@@ -103,8 +102,8 @@ smote_ipf <- function(data_origin, target, n=9, k=3, p=0.01, silent=TRUE) {
     rm(p_k_i)
   }
   
-  colnames(E)[colnames(E)=="smote_ipf_target"] <- target
-  return(E)
+  colnames(data)[colnames(data)=="smote_ipf_target"] <- colname_target
+  return(data)
 }
 
 #END
